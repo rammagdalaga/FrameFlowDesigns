c45be7bce3deb0e7b878ca2ca6503773e240bff0
Ram Magdalaga <73567718+rammagdalaga@users.noreply.github.com>
2024-12-02 15:05:52 +0800

    Delete menuBar.js

--- menuBar.js
+++
@@ -1,6 +0,0 @@
-const navBtn = document.querySelector('.menu-btn');
-const navList = document.querySelector('.nav-list');
-
-navBtn.addEventListener('click', function() {
-    navList.classList.toggle('active');
-});
